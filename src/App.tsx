4e34e293d749e8b1f929c95953c67b1f17f35843
duy <duy211099@gmail.com>
2023-07-29 16:22:18 +0700

    learn: test button components

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,76 @@ import { Accessibility, Airplay } from 'lucide-react'
 import { Button } from './components/Button'
 
 function App() {
-  return (
-    <>
-      <div>App</div>
-      <div className="h-20 space-x-1">
-        <Button
-          onClick={() => {
-            console.log('test onclick')
-          }}
-        >
-          DEMO
-        </Button>
-        <Button
-          startDecorator={<Accessibility size="10" />}
-          onClick={() => {
-            console.log('test onclick')
-          }}
-        >
-          DEMO
-        </Button>
-        <Button
-          endDecorator={<Airplay />}
-          onClick={() => {
-            console.log('test onclick')
-          }}
-        >
-          DEMO
-        </Button>
-        <Button
-          startDecorator={<Accessibility />}
-          endDecorator={<Airplay />}
-          onClick={() => {
-            console.log('test onclick')
-          }}
-        >
-          DEMO
-        </Button>
-      </div>
-    </>
-  )
+    return (
+        <div className="flex flex-col items-center space-y-4 p-2">
+            <div>App</div>
+            <ButtonContainer />
+        </div>
+    )
+}
+
+const ButtonContainer = () => {
+    const defaultOnClick = () => {
+        console.log('test onclick')
+    }
+    const asynOnClick = async () => {
+        console.log('clicked')
+        await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    }
+    const renderDefault = () => {
+        return (
+            <div className="space-x-1">
+                <Button onClick={defaultOnClick}>Default</Button>
+                <Button
+                    startDecorator={<Accessibility size="10" />}
+                    onClick={defaultOnClick}
+                >
+                    Default
+                </Button>
+                <Button endDecorator={<Airplay />} onClick={defaultOnClick}>
+                    Default
+                </Button>
+                <Button
+                    startDecorator={<Accessibility />}
+                    endDecorator={<Airplay />}
+                    onClick={defaultOnClick}
+                >
+                    Default
+                </Button>
+            </div>
+        )
+    }
+
+    const renderAsync = () => {
+        return (
+            <div className="space-x-1">
+                <Button onClick={asynOnClick}>Async</Button>
+                <Button
+                    startDecorator={<Accessibility size="10" />}
+                    onClick={asynOnClick}
+                >
+                    Async
+                </Button>
+                <Button endDecorator={<Airplay />} onClick={asynOnClick}>
+                    Async
+                </Button>
+                <Button
+                    startDecorator={<Accessibility />}
+                    endDecorator={<Airplay />}
+                    onClick={asynOnClick}
+                >
+                    Async
+                </Button>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {renderDefault()}
+            {renderAsync()}
+        </>
+    )
 }
 
 export default App
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,61 @@
+import { useState } from 'react'
+import { wait } from '../../utils'
+
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  startDecorator?: React.ReactNode
-  endDecorator?: React.ReactNode
-  loading?: boolean
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    startDecorator?: React.ReactNode
+    endDecorator?: React.ReactNode
+    loading?: boolean
 }
 
-export const Button = (props: ButtonProps) => {
-  const { startDecorator, endDecorator, children } = props
+export const Button = ({
+    startDecorator,
+    endDecorator,
+    children,
+    onClick,
+    ...props
+}: ButtonProps) => {
+    const [loading, setLoading] = useState(false)
 
-  return (
-    <button className="inline-flex flex-row content-center items-center gap-2 rounded-lg bg-primary-700 px-3 py-2 align-top text-sm text-white">
-      {startDecorator && <span className="btn-icon">{startDecorator}</span>}
-      {children}
-      {endDecorator && <span className="btn-icon">{endDecorator}</span>}
-    </button>
-  )
+    const isAsyncFunction = onClick?.constructor.name === 'AsyncFunction'
+
+    const onClickHandler = isAsyncFunction
+        ? (e) => {
+              setLoading(true)
+              Promise.all([onClick(e), wait()]).then(() => {
+                  setLoading(false)
+              })
+          }
+        : onClick
+
+    if (loading) {
+        startDecorator = (
+            <svg
+                fill="currentColor"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+                className="animate-spin text-xl"
+            >
+                <g>
+                    <path d="M8,1V2.8A5.2,5.2,0,1,1,2.8,8H1A7,7,0,1,0,8,1Z" />
+                </g>
+            </svg>
+        )
+    }
+
+    return (
+        <button
+            {...props}
+            onClick={onClickHandler}
+            className="inline-flex flex-row content-center items-center gap-2 rounded-lg bg-primary-700 px-3 py-2 align-top text-xs text-white"
+        >
+            {startDecorator && (
+                <span className="btn-icon">{startDecorator}</span>
+            )}
+            {children}
+            {endDecorator && <span className="btn-icon">{endDecorator}</span>}
+        </button>
+    )
 }
